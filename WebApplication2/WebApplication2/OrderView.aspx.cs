4ff32ee3630a1b20893d70f623b97e741cf15f64
Willieodwyer <willieod666@live.com>
2016-03-31 23:12:57 +0100

    Added a catalogue

--- WebApplication2/WebApplication2/OrderView.aspx.cs
+++ WebApplication2/WebApplication2/OrderView.aspx.cs
@@ -19,13 +19,13 @@ namespace WebApplication2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Order sentOrder = (Order) Session["OrderOBJ"];
+            //Order sentOrder = (Order) Session["OrderOBJ"];
             //Response.Write(sentOrder.PrintOrder());
             try
             {
                 string strSQLconnection = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\werl\Documents\Visual Studio 2013\Projects\SystemsAnalysis\WebApplication2\WebApplication2\App_Data\Database.mdf;Integrated Security=True";
                 SqlConnection sqlConnection = new SqlConnection(strSQLconnection);
-                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [Order] WHERE OrderID = " + Session["ID"].ToString(), sqlConnection);
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [Order] ORDER BY OrderID" , sqlConnection);// WHERE OrderID = " + Session["ID"].ToString(), sqlConnection);
                 sqlConnection.Open();
 
                 SqlDataReader reader = sqlCommand.ExecuteReader();
@@ -34,7 +34,7 @@ namespace WebApplication2
                 GridView1.DataBind();
                 //Response.Write(Session["ID"].ToString());
             }
-            catch (SqlException sept)
+            catch (Exception sept)
             {
                 Response.Write(sept.Message);
             }
