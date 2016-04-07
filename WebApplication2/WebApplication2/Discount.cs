dfd9008f9dcf36ddea08888be8447b4f486aac9a
Willieodwyer <willieod666@live.com>
2016-04-07 09:57:56 +0100

    Update Discount.cs

--- WebApplication2/WebApplication2/Discount.cs
+++ WebApplication2/WebApplication2/Discount.cs
@@ -1,17 +1,52 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Web.Configuration;
+using System.Text;
 
+//@"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\workarea\SystemsAnalysis-master\SystemsAnalysis-master\WebApplication2\WebApplication2\App_Data\Database2.mdf;Integrated Security=True"
 namespace WebApplication2
 {
     public class Discount
     {
         public int customerClass { get; set; }
-        public int productID { get; set; }
+        public List<int> productList { get; set; }
+        public List<int> customerList { get; set; }
+        public List<double> discountList { get; set; }
 
         public Discount(int custClass)
         {
+            try
+            {
+                string strSQLconnection = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\workarea\SystemsAnalysis-master\SystemsAnalysis-master\WebApplication2\WebApplication2\App_Data\Database2.mdf;Integrated Security=True";
+                SqlConnection sqlConnection = new SqlConnection(strSQLconnection);
+                SqlCommand sqlCommand = new SqlCommand("SELECT * FROM [Discount]";
+                sqlConnection.Open();
+
+                SqlDataReader reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    //OrderID	CustomerID	ProductID	Address	Amount	OrderDate	SupplierID
+                    productList.Add(Convert.ToInt32(reader["ProductID"]));
+                    customerList.Add(Convert.ToInt32(reader["CustomerID"]));
+                    discountList.Add(Convert.ToDouble(reader["discount"]));
+                }
+            }
+            catch (SqlException sept)
+            {
+                Console.WriteLine(sept.Message);
+            }
+
             customerClass = custClass;
         }
 
