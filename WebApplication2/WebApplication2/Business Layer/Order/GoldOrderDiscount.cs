0067cab09e5dc22627cbfbb3bcc21b068bdc5eda
Willieodwyer <willieod666@live.com>
2016-04-22 15:24:16 +0100

    Fixed state pattern

---
+++ WebApplication2/WebApplication2/Business Layer/Order/GoldOrderDiscount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Text.RegularExpressions;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Web.Configuration;
+using System.Text;
+
+
+
+namespace WebApplication2
+{
+    public class GoldOrderDiscount : OrderDiscount
+    {
+
+        override public double disc { get; set; }
+        public GoldOrderDiscount()
+        {
+            disc = 0.5;
+        }
+
+        override public void applyDiscount(Order ord)
+        {
+            ord.Amount = ord.Amount * disc;
+        }
+        
+    }
+}
